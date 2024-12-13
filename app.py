13767eeba19c51bfa557316e90298610e30b6678
Melvin-Jovano <melvinjovano2@gmail.com>
2024-12-14 04:43:30 +0700

    feat: disable debug mode

--- app.py
+++ app.py
@@ -158,4 +158,4 @@ def menu_recommendation(text, user_id):
     emit('menu_recommendation_response', recommendation)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8000, host='0.0.0.0')
+    socketio.run(app, port=8000, host='0.0.0.0')
