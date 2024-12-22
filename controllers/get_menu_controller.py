522fb449d01da994bc72039afea522bd4e1b1237
Melvin-Jovano <melvinjovano2@gmail.com>
2024-12-22 19:13:42 +0700

    feat: add response

--- controllers/get_menu_controller.py
+++ controllers/get_menu_controller.py
@@ -3,5 +3,5 @@ from services.menus import menu_by_id, menus
 
 def get_menu_controller(id: str):
     if id == 'all':
-        return menus('')
+        return response(menus(''))
     return response(menu_by_id(id))
