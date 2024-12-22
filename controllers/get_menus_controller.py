badbb52f4c270f036e70268af0af8974a7154d05
Melvin-Jovano <melvinjovano2@gmail.com>
2024-12-22 17:45:17 +0700

    feat: traceback

--- controllers/get_menus_controller.py
+++ controllers/get_menus_controller.py
@@ -3,34 +3,39 @@ from services.menus import menus_by_ids, menus_by_not_in_ids, menus
 from services.categories import get_categories
 from models.Menu import MenuList
 from services.menu_categories import get_menu_category_by_category_id
+import traceback
 
 def get_menus_controller(keyword: str):
-    if keyword:
-        return response(menus(keyword))
-    
-    result = []
-    stored_menu_ids = []
-    
-    for category in get_categories():
-        menu_ids = []
-        for menu_categories in get_menu_category_by_category_id(category['id']):
-            menu_ids.append(menu_categories['menu_id'])
-            stored_menu_ids.append(menu_categories['menu_id'])
+    try:
+        if keyword:
+            return response(menus(keyword))
+        
+        result = []
+        stored_menu_ids = []
+        
+        for category in get_categories():
+            menu_ids = []
+            for menu_categories in get_menu_category_by_category_id(category['id']):
+                menu_ids.append(menu_categories['menu_id'])
+                stored_menu_ids.append(menu_categories['menu_id'])
 
-        items = menus_by_ids(menu_ids)
-        if len(items) > 0:
-            result.append(
-                MenuList(
-                    category_name = category['name'],
-                    items = items
-                ).model_dump()
+            items = menus_by_ids(menu_ids)
+            if len(items) > 0:
+                result.append(
+                    MenuList(
+                        category_name = category['name'],
+                        items = items
+                    ).model_dump()
+            )
+
+        result.append(
+            MenuList(
+                category_name = "Other",
+                items = menus_by_not_in_ids(stored_menu_ids)
+            ).model_dump()
         )
 
-    result.append(
-        MenuList(
-            category_name = "Other",
-            items = menus_by_not_in_ids(stored_menu_ids)
-        ).model_dump()
-    )
-
-    return response(result, "SUCCESS", False)
+        return response(result, "SUCCESS", False)
+    except Exception as e:
+        error_details = traceback.format_exc()
+        return response({"error": str(e), "details": error_details}, "ERROR", True)
