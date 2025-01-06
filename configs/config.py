8dd78a34423845661a98e762ce25ce3e6ec1eeda
Melvin-Jovano <melvinjovano2@gmail.com>
2025-01-06 11:00:23 +0700

    feat: CORS

--- configs/config.py
+++ configs/config.py
@@ -5,7 +5,7 @@ DB_NAME = dotenv_values(".env")['DB_NAME']
 SESSION_SECRET = dotenv_values(".env")['SESSION_SECRET']
 SESSION_ADMIN_SECRET = dotenv_values(".env")['SESSION_ADMIN_SECRET']
 SESSION_EXPIRED_IN_MINUTES = 15
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://ner-frontend-client.vercel.app"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5174", "http://localhost:5173", "https://ner-frontend-client.vercel.app", "https://ryomu-admin.vercel.app"]
 MT_SERVER_ID = dotenv_values(".env")['MT_SERVER_ID']
 DEBUG_MODE = True if dotenv_values(".env")['DEBUG_MODE'] == "true" else False
 CLIENT_WEB_URL = 'https://ner-frontend-client.vercel.app'
